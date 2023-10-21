f30077aeb2e19679c3204fdf7c37f7bf265f6b13
yapduser <apronyaev@yandex.ru>
2023-10-22 01:23:05 +0300

    chore: 15

--- backend/run-up.sh
+++ backend/run-up.sh
@@ -8,4 +8,4 @@ python manage.py migrate
 python manage.py collectstatic
 cp -r /app/collected_static/. web/backend_static/static
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')" | python manage.py shell
-gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
+gunicorn --bind 0.0.0.0:8000 kittygram_backend.wsgi
